6c9d788c737e4800cafe9d459f74597b9fe44925
jkClyde <aquin.randall@gmail.com>
2025-02-12 03:57:35 +0800

    added list page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const HomePage = () => {
   return (
     <div className=''>
       <Slider/>
-      <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-xl">Featured Products</h1>
         <ProductList />
       </div>
@@ -15,9 +15,15 @@ const HomePage = () => {
         <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
           Categories
         </h1>
-    
           <CategoryList />
       </div>
+
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h1 className="text-xl">New Products</h1>
+        <ProductList />
+      </div>
+
+     
     </div>
   )
 }
