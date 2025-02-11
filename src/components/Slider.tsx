c80c7073dcaa1a8fbbe09f1f2ff5ae1bc08930b7
jkClyde <aquin.randall@gmail.com>
2025-02-12 06:57:35 +0800

    update

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -80,7 +80,7 @@ const Slider = () => {
           </div>
         ))}
       </div>
-      <div className="absolute m-auto left-[37%] sm:left-1/2 bottom-8 flex gap-4">
+      <div className="absolute m-auto left-[39%] sm:left-1/2 bottom-8 flex gap-4">
         {slides.map((slide, index) => (
           <div
             className={`w-3 h-3  rounded-full ring-1 ring-white sm:ring-gray-600 cursor-pointer flex items-center justify-center ${
