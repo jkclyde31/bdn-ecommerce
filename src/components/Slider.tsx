965f9691d2dd2bb8b82c1ff646ae1b08eba72cd0
jkClyde <aquin.randall@gmail.com>
2025-02-12 06:57:15 +0800

    slider bullets update

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -80,17 +80,17 @@ const Slider = () => {
           </div>
         ))}
       </div>
-      <div className="absolute m-auto left-1/2 bottom-8 flex gap-4">
+      <div className="absolute m-auto left-[37%] sm:left-1/2 bottom-8 flex gap-4">
         {slides.map((slide, index) => (
           <div
-            className={`w-3 h-3  rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
+            className={`w-3 h-3  rounded-full ring-1 ring-white sm:ring-gray-600 cursor-pointer flex items-center justify-center ${
               current === index ? "scale-150" : ""
             }`}
             key={slide.id}
             onClick={() => setCurrent(index)}
           >
             {current === index && (
-              <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
+              <div className="w-[6px] h-[6px] bg-white sm:bg-gray-600 rounded-full"></div>
             )}
           </div>
         ))}
