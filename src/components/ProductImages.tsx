ff8f46d3fb89b0c8c47a173cc088b2c318b25a6e
jkClyde <aquin.randall@gmail.com>
2025-02-12 04:19:17 +0800

    achuchu

---
+++ src/components/ProductImages.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+const images = [
+  {
+    id: 1,
+    url: "https://images.pexels.com/photos/30666404/pexels-photo-30666404/free-photo-of-dynamic-ocean-waves-captured-from-above.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 2,
+    url: "https://images.pexels.com/photos/17417677/pexels-photo-17417677/free-photo-of-crescent-on-clear-sky-over-man-walking-with-lanter-on-shore-at-night.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+  },
+  {
+    id: 3,
+    url: "https://images.pexels.com/photos/21812160/pexels-photo-21812160/free-photo-of-puerta-colonial-color-rojo-de-guanajuato-mexico.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+  },
+  {
+    id: 4,
+    url: "https://images.pexels.com/photos/20832069/pexels-photo-20832069/free-photo-of-a-narrow-street-with-buildings-and-cars.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load",
+  },
+];
+
+const ProductImages = () => {
+  const [index, setIndex] = useState(0);
+
+  return (
+    <div className="">
+      <div className="h-[500px] relative">
+        <Image
+          src={images[1].url}
+          alt=""
+          fill
+          sizes="50vw"
+          className="object-cover rounded-md"
+        />
+      </div>
+      <div className="flex justify-between gap-4 mt-8">
+        {images.map((item:any, i:number) => (
+          <div
+            className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer"
+            key={item._id}
+            onClick={() => setIndex(i)}
+          >
+            <Image
+              src={item.url}
+              alt=""
+              fill
+              sizes="30vw"
+              className="object-cover rounded-md"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductImages;
