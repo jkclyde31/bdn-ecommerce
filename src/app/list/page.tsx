897d78c95e77d00ce5ee5a9a6152ef13cc2792a9
jkClyde <aquin.randall@gmail.com>
2025-02-14 07:46:49 +0800

    added skeleton

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,15 +1,16 @@
 import Filter from "@/components/Filter";
 import ProductList from "@/components/ProductList";
-// import Skeleton from "@/components/Skeleton";
-// import { wixClientServer } from "@/lib/wixClientServer";
+import Skeleton from "@/components/Skeleton";
+import { wixClientServer } from "../../../lib/wixClientServer";
 import Image from "next/image";
+import { Suspense } from "react";
 
-const ListPage = async () => {
-  // const wixClient = await wixClientServer();
+const ListPage = async ({ searchParams }: { searchParams: any }) => {
+  const wixClient = await wixClientServer();
 
-  // const cat = await wixClient.collections.getCollectionBySlug(
-  //   searchParams.cat || "all-products"
-  // );
+  const cat = await wixClient.collections.getCollectionBySlug(
+    searchParams.cat || "all-products"
+  );
 
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
@@ -31,13 +32,15 @@ const ListPage = async () => {
       {/* FILTER */}
       <Filter />
       {/* PRODUCTS */}
-      <h1 className="mt-12 text-xl font-semibold"> For You!</h1>
-      {/* <Suspense fallback={<Skeleton/>}> */}
+      <h1 className="mt-12 text-xl font-semibold">{cat?.collection?.name} For You!</h1>
+      <Suspense fallback={<Skeleton/>}>
         <ProductList
           categoryId={
-            "00000000-000000-000000-000000000001"
+            cat.collection?._id || "00000000-000000-000000-000000000001"
           }
+          searchParams={searchParams}
         />
+      </Suspense>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,70 @@
+// "use client";
 
-import ProductList from "@/components/ProductList"
-import Slider from "@/components/Slider"
-import CategoryList from "@/components/CategoryList"
-import { Suspense } from "react"
-import { WixClientContext } from "@/context/wixContext"
-import { wixClientServer } from "../../lib/wixClientServer"
+import CategoryList from "@/components/CategoryList";
+import ProductList from "@/components/ProductList";
+import Skeleton from "@/components/Skeleton";
+import Slider from "@/components/Slider";
+import { WixClientContext } from "@/context/wixContext";
+import { useWixClient } from "../../hooks/useWixClient";
+import { wixClientServer } from "../../lib/wixClientServer";
+import { Suspense, useContext, useEffect } from "react";
 
-const HomePage =  () => {
+const HomePage = async () => {
 
-  // const wixClient = useContext(WixClientContext)
+  // TEST (FETCHING ON THE CLIENT COMPONENT)
 
-  // useEffect(()=>{
-  //   const getProducts = async () =>{
+  // const wixClient = useWixClient()
+
+  // useEffect(() => {
+  //   const getProducts = async () => {
   //     const res = await wixClient.products.queryProducts().find();
-  //     console.log("RESULTS: ", res)
-  //   }
+
+  //     console.log(res)
+  //   };
+
   //   getProducts();
+  // }, [wixClient]);
   
-  // }, [wixClient])
+
+  // TEST (FETCHING ON THE SERVER COMPONENT)
 
   // const wixClient = await wixClientServer();
-  // const res =  await wixClient.products.queryProducts().find();
-  // console.log("Result", res)
 
+  // const res = await wixClient.products.queryProducts().find();
+
+  // console.log(res);
 
   return (
-    <div className=''>
-      <Slider/>
+    <div className="">
+      <Slider />
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h1 className="text-xl">Featured Products</h1>
-        <Suspense fallback={"Loading..."}>
-           <ProductList categoryId="4cf9beca-f03b-09fa-d929-c10465cfee57" limit={4}/>
+        <h1 className="text-2xl">Featured Products</h1>
+        <Suspense fallback={<Skeleton />}>
+          <ProductList
+            categoryId="57bb1317-2426-a42b-e796-333626bb7b79"
+            limit={4}
+          />
         </Suspense>
       </div>
-
       <div className="mt-24">
         <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
           Categories
         </h1>
+        <Suspense fallback={<Skeleton />}>
           <CategoryList />
+        </Suspense>
       </div>
-
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h1 className="text-xl">New Products</h1>
-        {/* <ProductList /> */}
+        <h1 className="text-2xl">New Products</h1>
+        {/* <Suspense fallback={<Skeleton />}>
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_NEW_CATEGORY_ID!}
+            limit={4}
+          />
+        </Suspense> */}
       </div>
-
-     
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
