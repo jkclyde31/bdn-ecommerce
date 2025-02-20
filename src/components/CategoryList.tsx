7761db50b6cdb7896dc0a09c063f59e35074dd0f
jkClyde <aquin.randall@gmail.com>
2025-02-21 02:48:08 +0800

    Categories List Re Design

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -4,25 +4,24 @@ import Link from "next/link";
 
 const CategoryList = async () => {
   const wixClient = await wixClientServer();
-
   const cats = await wixClient.collections.queryCollections().find();
 
   return (
-    <div className="px-4 overflow-x-scroll scrollbar-hide">
-      <div className="flex gap-4 md:gap-8">
+    <div className="px-4 mx-auto max-w-7xl">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4  gap-4 md:gap-8">
         {cats.items.map((item) => (
           <Link
             href={`/list?cat=${item.slug}`}
-            className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+            className="block group"
             key={item._id}
           >
-            <div className="relative bg-slate-100 w-full h-96">
+            <div className="relative bg-slate-100 w-full aspect-[3/4]">
               <Image
                 src={item.media?.mainMedia?.image?.url || "/cat.png"}
                 alt=""
                 fill
-                sizes="20vw"
-                className="object-cover"
+                sizes="(min-width: 1536px) 16vw, (min-width: 1024px) 25vw, (min-width: 640px) 50vw, 100vw"
+                className="object-cover transition-transform duration-300 group-hover:scale-105"
               />
             </div>
             <h1 className="mt-8 font-light text-xl tracking-wide">
