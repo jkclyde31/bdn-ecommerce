35218f7496950e48ec7a52ba6ec94fd51f8222e6
jkClyde <aquin.randall@gmail.com>
2025-02-21 06:42:58 +0800

    fixed

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -4,6 +4,7 @@ import ProductImages from "@/components/ProductImages";
 // import Reviews from "@/components/Reviews";
 import { wixClientServer } from "../../../lib/wixClientServer";
 import { notFound } from "next/navigation";
+import DOMPurify from "isomorphic-dompurify";
 import { Suspense } from "react";
 
 const SinglePage = async ({ params }: { params: { slug: string } }) => {
@@ -29,7 +30,15 @@ const SinglePage = async ({ params }: { params: { slug: string } }) => {
       {/* TEXTS */}
       <div className="w-full lg:w-1/2 flex flex-col gap-6">
         <h1 className="text-4xl font-medium">{product.name}</h1>
-        <p className="text-gray-500">{product.description}</p>
+        {/* <p className="text-gray-500">{product.description}</p> */}
+        
+        <div
+              className="text-sm text-gray-500"
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(product.description || "")
+
+              }}
+        ></div>
         <div className="h-[2px] bg-gray-100" />
         {product.price?.price === product.price?.discountedPrice ? (
           <h2 className="font-medium text-2xl">${product.price?.price}</h2>
@@ -61,7 +70,17 @@ const SinglePage = async ({ params }: { params: { slug: string } }) => {
         {product.additionalInfoSections?.map((section: any) => (
           <div className="text-sm" key={section.title}>
             <h4 className="font-medium mb-4">{section.title}</h4>
-            <p>{section.description}</p>
+            {/* <p>{section.description}</p> */}
+         
+        
+
+            <div
+              className="text-sm text-gray-500"
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(section.description || "")
+
+              }}
+            ></div>
           </div>
         ))}
         <div className="h-[2px] bg-gray-100" />
