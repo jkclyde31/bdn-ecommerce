c4e99abebe8cd537f695b3cf12d7a6e1dddd1821
jkClyde <aquin.randall@gmail.com>
2025-03-02 04:10:24 +0800

    orders page

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,34 +1,6 @@
-"use client";
-
-
 import React from 'react'
-import { WixClientContext } from '@/context/wixContext'
-import { useWixClient } from '../../../hooks/useWixClient';
-import { useEffect } from 'react';
 
-const page =  () => {
-
-  const wixClient = useWixClient()
-
-  useEffect(() => {
-    console.log("Wix Client:", wixClient);
-    const getOrders = async () => {
-      try {
-        const orders = await wixClient.orders.queryDataItems({
-          dataCollectionId: "orders",
-        });
-        console.log("Orders", orders);
-      } catch (error) {
-        console.error("Error fetching orders:", error);
-      }
-    };
-  
-    getOrders();
-  }, [wixClient]);
-
-
-
-
+const page = () => {
   return (
     <div>page</div>
   )
